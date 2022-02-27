d15a96116e287dec69f2268b3713dc05dac433c2
huytq <huytq000605@gmail.com>
2022-02-27 23:17:53 +0700

    Fetch data

--- main.go
+++ main.go
@@ -2,26 +2,28 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"strconv"
 )
 
-func leetcode_question() url.Values {
-	return url.Values{
-		"operationName": {"questionData"},
-	}
+type questionType struct {
+	content    string
+	title      string
+	difficulty string
+	topics     []string
+	hints      []string
 }
 
 func main() {
-	leetcodeUrl := "https://leetcode.com/graphql"
-	js := []byte(`
-	{"operationName":"questionData","variables":{"titleSlug":"maximum-width-of-binary-tree"},"query":"query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    exampleTestcases\n    categoryTitle\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      paidOnly\n      hasVideoSolution\n      paidOnlyVideo\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    enableDebugger\n    envInfo\n    libraryUrl\n    adminUrl\n    challengeQuestion {\n      id\n      date\n      incompleteChallengeCount\n      streakCount\n      type\n      __typename\n    }\n    __typename\n  }\n}\n"}
-	`)
-	req, err := http.NewRequest("POST", leetcodeUrl, bytes.NewBuffer(js))
-	req.Header.Add("referer", "https://leetcode.com/problems/maximum-width-of-binary-tree/")
+	leetcode := "https://leetcode.com/graphql"
+	puzzle := "sliding-window-maximum"
+	query := []byte(fmt.Sprintf(`
+	{"operationName":"questionData","variables":{"titleSlug":"%s"},"query":"query questionData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n  title\n  content\n  difficulty\n   topicTags { name\n }\n   hints\n }\n}\n"}
+	`, puzzle))
+	req, err := http.NewRequest("POST", leetcode, bytes.NewBuffer(query))
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Content-Length", strconv.FormatInt(req.ContentLength, 10))
 
@@ -35,5 +37,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(body))
+
+	var responseMap map[string]interface{}
+	json.Unmarshal(body, &responseMap)
+
+	questionInResponse := responseMap["data"].(map[string]interface{})["question"].(map[string]interface{})
+
+	question := questionType{}
+
+	question.content = questionInResponse["content"].(string)
+	question.difficulty = questionInResponse["difficulty"].(string)
+	question.title = questionInResponse["title"].(string)
+
+	topicTags := questionInResponse["topicTags"].([]interface{})
+	question.topics = make([]string, len(topicTags))
+	for idx, topicTag := range topicTags {
+		question.topics[idx] = topicTag.(map[string]interface{})["name"].(string)
+	}
+
+	hints := questionInResponse["hints"].([]interface{})
+	question.hints = make([]string, len(hints))
+	for idx, hint := range hints {
+		question.hints[idx] = hint.(string)
+	}
+
+	fmt.Println(question)
+
 }
